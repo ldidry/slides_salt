20f551d774dc49e3561b1156c3d15f055de80ecb
Luc Didry <luc@didry.org>
2013-11-27 15:23:19 +0100

    Initial commit

---
+++ js/tag.js
@@ -0,0 +1,19 @@
+window.onload = function() {
+    Reveal.addEventListener( 'gogogo', function() {
+        console.log('toto');
+        try {
+            TagCanvas.Start('myCanvas', '', {
+                initial: [0.1,-0.1],
+                textHeight: 40,
+                textColour: '#000000',
+                textFont: '"Open Sans",sans-serif'
+            });
+        } catch(e) {
+            document.getElementById('myCanvasContainer').style.display = 'none';
+        }
+    });
+    Reveal.addEventListener( 'questions', function() {
+        document.getElementById('knock').play();
+    });
+};
+
